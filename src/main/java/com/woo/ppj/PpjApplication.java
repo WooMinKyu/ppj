7be0d71422be2647f22a40f537a26bf9dc9ea8af
MinKyu Woo <wooh4629@naver.com>
2023-05-23 16:37:01 +0900

    Inint Commit

---
+++ src/main/java/com/woo/ppj/PpjApplication.java
@@ -0,0 +1,13 @@
+package com.woo.ppj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woo/ppj/PpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woo.ppj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
